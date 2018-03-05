42157ba5a2fb252bba85b4c4dbd3a49f88c8ec50
Vitalii <36969980+Vitalik0007@users.noreply.github.com>
2018-03-06 02:54:31 +0300

    Add files via upload

---
+++ My own List/MyProject1.cpp
@@ -0,0 +1,84 @@
+#include "Vitalik.h"
+
+Container::~Container()
+{
+	while (begin != NULL)
+	{
+		Node* del = begin;
+		begin = begin->next;
+		delete del;
+	}
+}
+
+void Container::show()
+{
+	Node* bef = begin;
+	while (bef != NULL)
+	{
+		cout << bef->data << ", ";
+		bef = bef->next;
+	}
+	cout << "->NULL" << endl;
+}
+
+void Container::push(int el)
+{
+	Node* temp = new Node(el, begin);
+	if (begin == NULL || begin->data >= el) begin = temp;
+	else
+	{
+		Node* bef = begin;
+		Node* bef2 = 0;
+		while (bef != NULL && bef->data < el)
+		{
+			bef2 = bef;
+			bef = bef->next;
+		}
+		bef2->next = temp;
+		temp->next = bef;
+	}
+}
+
+void Container::pop()
+{
+	Node * del = 0;
+	Node * bef2 = 0;
+	Node * bef = begin;
+	while (bef != NULL && bef->next != NULL && bef->next->next != NULL && bef->next->next->next != NULL)
+	{
+		if (bef->next->data != bef->next->next->data)
+		{
+			if (bef == begin)
+			{
+				for (int i = 0; i<4; i++)
+				{
+					del = begin;
+					begin = begin->next;
+					delete del;
+				}
+				bef = begin;
+			}
+			else
+			{
+				for (int i = 0; i<4; i++)
+				{
+					del = bef;
+					bef2->next = bef->next;
+					bef = bef->next;
+					delete del;
+				}
+			}
+		}
+		else
+		{
+			for (int i = 0; i<2; i++)
+			{
+				del = bef->next;
+				bef->next = bef->next->next;
+				delete del;
+			}
+			bef2 = bef->next;
+			bef = bef2->next;
+		}
+	}
+}
